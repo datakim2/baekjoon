952fe3508faeeb2934743e2ccd1a555c72dc891d
datakim <hiehsqjfrl@gmail.com>
2022-04-04 15:53:55 +0900

    Create Main.java

---
+++ baekjoon11403/Main.java
@@ -0,0 +1,45 @@
+package baekjoon11403;
+
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = stoi(br.readLine());
+
+		int[][] map = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
+			String[] s = br.readLine().split(" ");
+			for (int j = 0; j < N; j++) {
+				map[i][j] = stoi(s[j]);
+			}
+		}
+
+		for (int k = 0; k < N; k++) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (map[i][k] == 1 && map[k][j] == 1) {
+						map[i][j] = 1;
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				sb.append(map[i][j] + " ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+
+	}
+
+	public static int stoi(String s) {
+		return Integer.parseInt(s);
+	}
+
+}
