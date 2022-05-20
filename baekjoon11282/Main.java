5b39279f69563950d3d8d584abaddb03c9d03aca
datakim2 <hiehsqjfrl@gmail.com>
2022-05-20 09:37:11 +0900

    Create Main.java

---
+++ baekjoon11282/Main.java
@@ -0,0 +1,14 @@
+package baekjoon11282;
+
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		char c = (char) (Integer.parseInt(br.readLine()) + 44031);
+
+		System.out.println(c);
+	}
+
+}
