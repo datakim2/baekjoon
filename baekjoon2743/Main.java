5e47d21742e4666e84a403b9fb918df7c086686b
김정현 <98461400+datakim2@users.noreply.github.com>
2022-06-07 11:33:12 +0900

    Create Main.java

---
+++ baekjoon2743/Main.java
@@ -0,0 +1,13 @@
+package baekjoon2743;
+
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String word = br.readLine();
+		System.out.println(word.length());
+	}
+
+}
