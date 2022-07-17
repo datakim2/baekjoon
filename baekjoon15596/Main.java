263d364f8b327d10dea520214407aabc82b0f6a2
datakim2 <98461400+datakim2@users.noreply.github.com>
2022-07-17 20:52:07 +0900

    Create Main.java

---
+++ baekjoon15596/Main.java
@@ -0,0 +1,14 @@
+package baekjoon15596;
+
+public class Main {
+
+	public long sum(int[] a) {
+		long ans = 0;
+		for (int i = 0; i < a.length; i++) {
+			ans += a[i];
+		}
+		return ans;
+
+	}
+
+}
