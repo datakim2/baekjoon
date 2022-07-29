82ad72d68ba20c32db9b74b703e9f7eb9d1b8495
datakim2 <hiehsqjfrl@gmail.com>
2022-07-29 09:01:09 +0900

    Create Main.java

---
+++ baekjoon9086/Main.java
@@ -0,0 +1,28 @@
+package baekjoon9086;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int T = stoi(br.readLine());
+
+		for (int i = 0; i < T; i++) {
+			String word = br.readLine();
+			sb.append(word.charAt(0));
+			sb.append(word.charAt(word.length() - 1));
+			sb.append("\n");
+		}
+
+		System.out.println(sb.toString());
+	}
+
+	static int stoi(String s) {
+		return Integer.parseInt(s);
+	}
+
+}
